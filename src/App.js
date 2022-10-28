e45a84f2f95c00bcb89381144641e260cb6a751b
Anish Chaudhary <chaudharyanish305@gmail.com>
2022-10-28 14:18:27 +0545

    Initial project setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import TodoList from "./features/todos/TodoList";
 
 function App() {
   return (
     <div className="App">
-      rtk query
+      <TodoList />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import  {ApiProvider} from '@reduxjs/toolkit/query/react';
+import  {apiSlice} from './features/api/apiSlice'
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApiProvider api={apiSlice }>
+      <App />
+    </ApiProvider>
   </React.StrictMode>
 );
 
